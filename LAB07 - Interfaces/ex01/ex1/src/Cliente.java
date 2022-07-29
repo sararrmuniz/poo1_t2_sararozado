dc2088a48106214a3e2bc07d7006a17a4bca5f4e
sararrmuniz <sararozado@hotmail.com>
2022-07-29 13:47:55 -0300

    práticas

---
+++ LAB07 - Interfaces/ex01/ex1/src/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente implements Classificavel {
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean eMenorQue(Classificavel o) {
+        Cliente compara = (Cliente) o;
+        if (this.nome.compareTo(compara.nome) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void imprime(){
+        System.out.println("Vetor Ordenado: " + nome);
+    }
+}
